7e70df3a9c0bab5f0f90df31a86ac3324b643cdc
kedanco <kelvinchowjunjie120@hotmail.com>
2018-09-18 15:15:15 +0800

    configured mongo, passportJS,google Oauth20

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+	googleId: String
+});
+
+// create new collection called users
+// No overwrite, create if not existing
+mongoose.model("users", userSchema);
