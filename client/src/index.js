3d024c37b6a8310831609eeb61e90a41be7f722b
kedanco <kelvinchowjunjie120@hotmail.com>
2018-09-28 06:28:05 +0800

    Setup Stripe keys

--- client/src/index.js
+++ client/src/index.js
@@ -16,3 +16,5 @@ ReactDOM.render(
 	</Provider>,
 	document.querySelector("#root")
 );
+console.log("STRIKE KEY IS", process.env.REACT_APP_STRIPE_KEY);
+console.log("Environment is", process.env.NODE_ENV);
